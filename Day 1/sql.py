e1b750ae297b81ffa348629967741f635f449150
Akshyy31 <akshayshaji688@gmail.com>
2025-09-10 10:18:25 +0530

    first commit

---
+++ Day 1/sql.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PostgreSQL 
+# PostgreSQL is one of the most advanced Relational database management systems (RDBMS). 
+# It is open-source software, which means the source code is available under the PostgreSQL license. 
+
+# It supports both relational as well as Non-Relational JSON Queries.
